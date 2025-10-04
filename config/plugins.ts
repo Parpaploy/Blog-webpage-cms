2fa29a3d79a36fe9f0104767dc281b76c60ce148
parpaploy <ramita1846@gmail.com>
2025-10-04 16:17:01 +0700

    update

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("EMAIL_FROM_ADDRESS"),
+        defaultReplyTo: env("EMAIL_FROM_ADDRESS"),
+      },
+    },
+  },
+});
