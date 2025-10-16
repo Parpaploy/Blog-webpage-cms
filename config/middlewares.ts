2eb7d583cc663dc434ec22f8ffc31b9a863b58cb
parpaploy <ramita1846@gmail.com>
2025-10-16 11:33:32 +0700

    update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,11 +1,44 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http://localhost:3000"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: "strapi::cors",
     config: {
-      origin: ["http://localhost:3000"],
+      enabled: true,
+      origin: ["http://localhost:3000", "http://localhost:1337", "null"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "X-Frame-Options",
+        "access-control-allow-origin",
+      ],
+      credentials: true,
     },
   },
   "strapi::poweredBy",
